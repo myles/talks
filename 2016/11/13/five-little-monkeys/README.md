9678d7961864eb84c462653c243461618e2cfe45
Myles Braithwaite <me@mylesbraithwaite.com>
2016-11-17 15:10:04 -0500

    Working on a Travis deploy.

---
+++ 2016/11/13/five-little-monkeys/README.md
@@ -0,0 +1,10 @@
+---
+title: "Five Little Monkeys"
+location: Ted Rogers School of Management at Ryerson University
+date: 2016-11-13
+for:
+  name: PyCon Canada 2016
+  link: https://2016.pycon.ca/en/
+---
+
+I gave a lighting talk on my programming exercise [Five Little Monkeys](https://github.com/myles/five-little-monkeys).
---
+++ _pages/build.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+Converts the README.md files to html for the GitHub Pages.
+
+Requirments:
+- markdown
+- pyyaml
+"""
+
+import re
+import yaml
+import codecs
+from os import chdir
+from glob import iglob
+from os.path import dirname, realpath, join
+
+from markdown import markdown
+
+readme_regex = re.compile(r'(?s)(---(?P<yaml>.*)---)(?P<markdown>.*)')
+
+
+def get_talk_contents(path):
+    with codecs.open(path, mode='r', encoding='utf-8') as fobj:
+        raw = fobj.read()
+
+    result = re.match(readme_regex, raw).groupdict()
+
+    talk = yaml.load(result['yaml'])
+
+    talk['description'] = markdown(result['markdown'].strip())
+
+    return talk
+
+
+def render_talk_page(talk, template, path):
+    with codecs.open(join(dirname(path), 'index.html'), mode='w',
+                     encoding='utf-8') as fobj:
+        fobj.write(template.format(**talk))
+
+
+def main():
+    current_dir = join(dirname(realpath(__file__)))
+
+    chdir(current_dir)
+    talk_files = iglob('../2016/**/README.md', recursive=True)
+
+    with codecs.open(join(current_dir, 'template.html'), mode='r',
+                     encoding='utf-8') as fobj:
+        tpl = fobj.read()
+
+    for talk_file_path in talk_files:
+        talk = get_talk_contents(talk_file_path)
+        render_talk_page(talk, tpl, talk_file_path)
+
+
+if __name__ == '__main__':
+    main()
